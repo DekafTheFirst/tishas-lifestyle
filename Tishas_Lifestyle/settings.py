fde88cd895fa69d77b50399a59ddbfdb8898d84d
Destiny Kefas <dekeji1@gmail.com>
2021-02-08 22:34:16 +0800

    hostt

--- Tishas_Lifestyle/settings.py
+++ Tishas_Lifestyle/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'm2cl(hp&9gs=e=kjv0w1!y6mdg29$t4r3-qzrm*)0p^q=6-#p#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['lildtk.pythonanywhere.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'lildtk.pythonanywhere.com/']
 
 
 # Application definition
